8d278a913e507f7fe6c47c7b3d045f6c9a788052
Edson G Maldonado <edsingm@hotmail.com>
2025-10-08 20:31:06 -0300

    refactor: comment out account creation link in Login component
    - Commented out the paragraph containing the link to create a new account in the Login component for potential future use or design changes.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -68,7 +68,7 @@ const Login: React.FC = () => {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Entre na sua conta
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
+          {/* <p className="mt-2 text-center text-sm text-gray-600">
             Ou{' '}
             <Link
               to="/register"
@@ -76,7 +76,7 @@ const Login: React.FC = () => {
             >
               crie uma nova conta
             </Link>
-          </p>
+          </p> */}
         </div>
 
         {/* Form */}
